4fd5a5ab45d9dd6c61031f817ae43f0e64503b2a
Boris Gnezdilov <sysashi.boris@gmail.com>
2017-02-10 23:56:37 +0800

    Add some stuff [non imp]

--- web/static/elm/Main.elm
+++ web/static/elm/Main.elm
@@ -1,4 +1,58 @@
-import Html
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing(onClick)
 
 
-main = Html.text "Hello World"
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+
+
+-- MODEL
+
+
+type alias Model = Int
+
+
+model : Model
+model =
+    0
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Increment
+    | Decrement
+    | Reset
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+        Reset ->
+            0
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Reset ] [ text "reset" ]
+        ]
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -20,5 +20,5 @@ import "phoenix_html"
 
 // import socket from "./socket"
 
-const Elm = require("../elm/Main")
-Elm.Main.embed(document.getElementById("elm-main"))
+// const Elm = require("../elm/Main")
+// Elm.Main.embed(document.getElementById("elm-main"))
